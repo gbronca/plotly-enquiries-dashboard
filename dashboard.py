c6d7642b881db62fbc59ad9044f6213a130fccac
gbronca <gbronca@gmail.com>
2020-04-11 19:27:04 +0100

    initial commit

---
+++ dashboard.py
@@ -0,0 +1,16 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import pandas as pd
+from datetime import datetime
+
+app = dash.Dash()
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run_server()
